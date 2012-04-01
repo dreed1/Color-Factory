2de8f3e28be931d93035884c7de8417a00b2fe56
Dan Reed <makkuronova@gmail.com>
2012-03-31 21:52:21 -0700

    wrote the readme and included the actual files for the factory

---
+++ ColorFactory.h
@@ -0,0 +1,88 @@
+//
+//  ColorFactory.h
+//
+//  Created by Dan Reed on 3/31/12.
+//  Copyright (c) 2012 danreed.net. All rights reserved.
+//
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <dan@danreed.net> wrote this class. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return --Dan Reed
+ * ----------------------------------------------------------------------------
+ */
+
+#define PRIMARY_COLOR @"FFCF9C"
+#define SECONDARY_COLOR @"8190C4"
+#define TERTIARY_COLOR @"7CCB9E"
+
+#define PRIMARY_SHADOW_COLOR @"BFA487"
+#define SECONDARY_SHADOW_COLOR @"6D7693"
+#define TERTIARY_SHADOW_COLOR @"6C987F"
+
+#define PRIMARY_BOLD_COLOR @"A66E33"
+#define SECONDARY_BOLD_COLOR @"2A3E7F"
+#define TERTIARY_BOLD_COLOR @"288450"
+
+#define PRIMARY_LIGHT_COLOR @"FFDBB5"
+#define SECONDARY_LIGHT_COLOR @"A7B5E2"
+#define TERTIARY_LIGHT_COLOR @"A2E5BF"
+
+#define PRIMARY_HIGHLIGHT_COLOR @"FFE4C8"
+#define SECONDARY_HIGHLIGHT_COLOR @"C7D1F2"
+#define TERTIARY_HIGHLIGHT_COLOR @"C4F5D9"
+
+#import <Foundation/Foundation.h>
+
+@interface ColorFactory : NSObject{
+    UIColor *primary;
+    UIColor *secondary;
+    UIColor *tertiary;
+    
+    UIColor *primaryShadow;
+    UIColor *secondaryShadow;
+    UIColor *tertiaryShadow;
+    
+    UIColor *primaryBold;
+    UIColor *secondaryBold;
+    UIColor *tertiaryBold;
+    
+    UIColor *primaryLight;
+    UIColor *secondaryLight;
+    UIColor *tertiaryLight;
+    
+    UIColor *primaryHighlight;
+    UIColor *secondaryHighlight;
+    UIColor *tertiaryHighlight;
+}
+
+@property (nonatomic, strong) UIColor *primary;
+@property (nonatomic, strong) UIColor *secondary;
+@property (nonatomic, strong) UIColor *tertiary;
+
+@property (nonatomic, strong) UIColor *primaryShadow;
+@property (nonatomic, strong) UIColor *secondaryShadow;
+@property (nonatomic, strong) UIColor *tertiaryShadow;
+
+@property (nonatomic, strong) UIColor *primaryBold;
+@property (nonatomic, strong) UIColor *secondaryBold;
+@property (nonatomic, strong) UIColor *tertiaryBold;
+
+@property (nonatomic, strong) UIColor *primaryLight;
+@property (nonatomic, strong) UIColor *secondaryLight;
+@property (nonatomic, strong) UIColor *tertiaryLight;
+
+@property (nonatomic, strong) UIColor *primaryHighlight;
+@property (nonatomic, strong) UIColor *secondaryHighlight;
+@property (nonatomic, strong) UIColor *tertiaryHighlight;
+
++(ColorFactory *)sharedFactory;
+
++ (UIColor *) colorFromHex:(NSString *)hex;
++ (UIColor *) colorFromRGB:(int)red:(int)green:(int)blue;
++ (UIColor *) colorFromRGBA:(int)red:(int)green:(int)blue:(float)alpha;
+
+
+@end
---
+++ ColorFactory.m
@@ -0,0 +1,121 @@
+//
+//  ColorFactory.m
+//
+//  Created by Dan Reed on 3/31/12.
+//  Copyright (c) 2012 danreed.net. All rights reserved.
+//
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <dan@danreed.net> wrote this class. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return --Dan Reed
+ * ----------------------------------------------------------------------------
+ */
+
+#import "ColorFactory.h"
+
+@implementation ColorFactory
+
+@synthesize primary;
+@synthesize secondary;
+@synthesize tertiary;
+
+@synthesize primaryShadow;
+@synthesize secondaryShadow;
+@synthesize tertiaryShadow;
+
+@synthesize primaryBold;
+@synthesize secondaryBold;
+@synthesize tertiaryBold;
+
+@synthesize primaryLight;
+@synthesize secondaryLight;
+@synthesize tertiaryLight;
+
+@synthesize primaryHighlight;
+@synthesize secondaryHighlight;
+@synthesize tertiaryHighlight;
+
++(ColorFactory *)sharedFactory {
+    static dispatch_once_t pred;
+    static ColorFactory *shared = nil;
+    
+    dispatch_once(&pred, ^{
+        shared = [[ColorFactory alloc] init];
+    });
+    return shared;
+}
+
+-(ColorFactory *)init{
+    self = [super init];
+    if(self){
+        //init global colors here
+        primary = [ColorFactory colorFromHex:PRIMARY_COLOR];
+        secondary = [ColorFactory colorFromHex:SECONDARY_COLOR];
+        tertiary = [ColorFactory colorFromHex:TERTIARY_COLOR];
+        
+        primaryShadow = [ColorFactory colorFromHex:PRIMARY_SHADOW_COLOR];
+        secondaryShadow = [ColorFactory colorFromHex:SECONDARY_SHADOW_COLOR];
+        tertiaryShadow = [ColorFactory colorFromHex:TERTIARY_SHADOW_COLOR];
+        
+        primaryBold = [ColorFactory colorFromHex:PRIMARY_BOLD_COLOR];
+        secondaryBold = [ColorFactory colorFromHex:SECONDARY_BOLD_COLOR];
+        tertiaryBold = [ColorFactory colorFromHex:TERTIARY_BOLD_COLOR];
+        
+        primaryLight = [ColorFactory colorFromHex:PRIMARY_LIGHT_COLOR];
+        secondaryLight = [ColorFactory colorFromHex:SECONDARY_LIGHT_COLOR];
+        tertiaryLight = [ColorFactory colorFromHex:TERTIARY_LIGHT_COLOR];
+        
+        primaryHighlight = [ColorFactory colorFromHex:PRIMARY_HIGHLIGHT_COLOR];
+        secondaryHighlight = [ColorFactory colorFromHex:SECONDARY_HIGHLIGHT_COLOR];
+        tertiaryHighlight = [ColorFactory colorFromHex:TERTIARY_HIGHLIGHT_COLOR];
+    }
+    return self;
+}
+
++ (UIColor *) colorFromHex:(NSString *)hex {
+    
+    NSString *colorString = [[hex uppercaseString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] ;  
+    if ([colorString length] < 6) return [UIColor grayColor];  
+    
+    if ([colorString hasPrefix:@"0X"]) colorString = [colorString substringFromIndex:2];  
+    if ([colorString hasPrefix:@"#"]) colorString = [colorString substringFromIndex:1]; 
+    if ([colorString length] != 6) return  [UIColor grayColor];  
+    
+    NSRange range;  
+    range.location = 0;  
+    range.length = 2;  
+    NSString *rString = [colorString substringWithRange:range];  
+    range.location += 2;  
+    NSString *gString = [colorString substringWithRange:range];  
+    range.location += 2;  
+    NSString *bString = [colorString substringWithRange:range];  
+
+    unsigned int red, green, blue;  
+    [[NSScanner scannerWithString:rString] scanHexInt:&red];  
+    [[NSScanner scannerWithString:gString] scanHexInt:&green];  
+    [[NSScanner scannerWithString:bString] scanHexInt:&blue];  
+    
+    return [UIColor colorWithRed:((float) red / 255.0f)  
+                           green:((float) green / 255.0f)  
+                            blue:((float) blue / 255.0f)  
+                           alpha:1.0f];  
+} 
+
++ (UIColor *) colorFromRGB:(int)red:(int)green:(int)blue{
+    return [UIColor colorWithRed:((float) red / 255.0f)  
+                           green:((float) green / 255.0f)  
+                            blue:((float) blue / 255.0f)  
+                           alpha:1.0f];  
+}
+
++ (UIColor *) colorFromRGBA:(int)red:(int)green:(int)blue:(float)alpha{
+    return [UIColor colorWithRed:((float) red / 255.0f)  
+                           green:((float) green / 255.0f)  
+                            blue:((float) blue / 255.0f)  
+                           alpha:alpha];  
+}
+
+@end
