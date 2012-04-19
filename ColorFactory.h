4796cc9bcda0dea766756cce9657ee267ddb5beb
Dan Reed <makkuronova@gmail.com>
2012-04-19 11:06:08 -0700

    last commit didnt actually work -- note to self: do not write objective-c code in textmate

--- ColorFactory.h
+++ ColorFactory.h
@@ -14,69 +14,55 @@
 * ----------------------------------------------------------------------------
 */
 
-#define PRIMARY_COLOR @"FFCF9C"
-#define SECONDARY_COLOR @"8190C4"
-#define TERTIARY_COLOR @"7CCB9E"
+#define PRIMARY_COLOR @"397285"
+#define SECONDARY_COLOR @"d49756"
 
-#define PRIMARY_SHADOW_COLOR @"BFA487"
-#define SECONDARY_SHADOW_COLOR @"6D7693"
-#define TERTIARY_SHADOW_COLOR @"6C987F"
+#define PRIMARY_SHADOW_COLOR @"3a6a79"
+#define SECONDARY_SHADOW_COLOR @"c28f59"
 
-#define PRIMARY_BOLD_COLOR @"A66E33"
-#define SECONDARY_BOLD_COLOR @"2A3E7F"
-#define TERTIARY_BOLD_COLOR @"288450"
+#define PRIMARY_BOLD_COLOR @"296275"
+#define SECONDARY_BOLD_COLOR @"bb7e3f"
 
-#define PRIMARY_LIGHT_COLOR @"FFDBB5"
-#define SECONDARY_LIGHT_COLOR @"A7B5E2"
-#define TERTIARY_LIGHT_COLOR @"A2E5BF"
+#define PRIMARY_LIGHT_COLOR @"6eacc1"
+#define SECONDARY_LIGHT_COLOR @"e9b681"
 
-#define PRIMARY_HIGHLIGHT_COLOR @"FFE4C8"
-#define SECONDARY_HIGHLIGHT_COLOR @"C7D1F2"
-#define TERTIARY_HIGHLIGHT_COLOR @"C4F5D9"
+#define PRIMARY_HIGHLIGHT_COLOR @"83b2c1"
+#define SECONDARY_HIGHLIGHT_COLOR @"e9c49c"
 
 #import <Foundation/Foundation.h>
 
 @interface ColorFactory : NSObject{
     UIColor *primary;
     UIColor *secondary;
-    UIColor *tertiary;
     
     UIColor *primaryShadow;
     UIColor *secondaryShadow;
-    UIColor *tertiaryShadow;
     
     UIColor *primaryBold;
     UIColor *secondaryBold;
-    UIColor *tertiaryBold;
     
     UIColor *primaryLight;
     UIColor *secondaryLight;
-    UIColor *tertiaryLight;
     
     UIColor *primaryHighlight;
     UIColor *secondaryHighlight;
-    UIColor *tertiaryHighlight;
 }
 
 @property (nonatomic, strong) UIColor *primary;
 @property (nonatomic, strong) UIColor *secondary;
-@property (nonatomic, strong) UIColor *tertiary;
+
 
 @property (nonatomic, strong) UIColor *primaryShadow;
 @property (nonatomic, strong) UIColor *secondaryShadow;
-@property (nonatomic, strong) UIColor *tertiaryShadow;
 
 @property (nonatomic, strong) UIColor *primaryBold;
 @property (nonatomic, strong) UIColor *secondaryBold;
-@property (nonatomic, strong) UIColor *tertiaryBold;
 
 @property (nonatomic, strong) UIColor *primaryLight;
 @property (nonatomic, strong) UIColor *secondaryLight;
-@property (nonatomic, strong) UIColor *tertiaryLight;
 
 @property (nonatomic, strong) UIColor *primaryHighlight;
 @property (nonatomic, strong) UIColor *secondaryHighlight;
-@property (nonatomic, strong) UIColor *tertiaryHighlight;
 
 +(ColorFactory *)sharedFactory;
 
--- ColorFactory.m
+++ ColorFactory.m
@@ -20,23 +20,18 @@
 
 @synthesize primary;
 @synthesize secondary;
-@synthesize tertiary;
 
 @synthesize primaryShadow;
 @synthesize secondaryShadow;
-@synthesize tertiaryShadow;
 
 @synthesize primaryBold;
 @synthesize secondaryBold;
-@synthesize tertiaryBold;
 
 @synthesize primaryLight;
 @synthesize secondaryLight;
-@synthesize tertiaryLight;
 
 @synthesize primaryHighlight;
 @synthesize secondaryHighlight;
-@synthesize tertiaryHighlight;
 
 +(ColorFactory *)sharedFactory {
     static dispatch_once_t pred;
@@ -54,23 +49,18 @@
         //init global colors here
         primary = [ColorFactory colorFromHex:PRIMARY_COLOR];
         secondary = [ColorFactory colorFromHex:SECONDARY_COLOR];
-        tertiary = [ColorFactory colorFromHex:TERTIARY_COLOR];
         
         primaryShadow = [ColorFactory colorFromHex:PRIMARY_SHADOW_COLOR];
         secondaryShadow = [ColorFactory colorFromHex:SECONDARY_SHADOW_COLOR];
-        tertiaryShadow = [ColorFactory colorFromHex:TERTIARY_SHADOW_COLOR];
         
         primaryBold = [ColorFactory colorFromHex:PRIMARY_BOLD_COLOR];
         secondaryBold = [ColorFactory colorFromHex:SECONDARY_BOLD_COLOR];
-        tertiaryBold = [ColorFactory colorFromHex:TERTIARY_BOLD_COLOR];
         
         primaryLight = [ColorFactory colorFromHex:PRIMARY_LIGHT_COLOR];
         secondaryLight = [ColorFactory colorFromHex:SECONDARY_LIGHT_COLOR];
-        tertiaryLight = [ColorFactory colorFromHex:TERTIARY_LIGHT_COLOR];
         
         primaryHighlight = [ColorFactory colorFromHex:PRIMARY_HIGHLIGHT_COLOR];
         secondaryHighlight = [ColorFactory colorFromHex:SECONDARY_HIGHLIGHT_COLOR];
-        tertiaryHighlight = [ColorFactory colorFromHex:TERTIARY_HIGHLIGHT_COLOR];
     }
     return self;
 }
@@ -78,11 +68,15 @@
 + (UIColor *) colorFromHex:(NSString *)hex {
     
     NSString *colorString = [[hex uppercaseString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] ;  
-    if ([colorString length] < 6) return [UIColor grayColor];  
+    if ([colorString length] < 6) 
+        return [UIColor grayColor];  
     
-    if ([colorString hasPrefix:@"0X"]) colorString = [colorString substringFromIndex:2];  
-    if ([colorString hasPrefix:@"#"]) colorString = [colorString substringFromIndex:1]; 
-    if ([colorString length] != 6) return  [UIColor grayColor];  
+    if ([colorString hasPrefix:@"0X"]) 
+        colorString = [colorString substringFromIndex:2];  
+    else if ([colorString hasPrefix:@"#"]) 
+        colorString = [colorString substringFromIndex:1]; 
+    else if ([colorString length] != 6) 
+        return  [UIColor grayColor];  
     
     NSRange range;  
     range.location = 0;  
@@ -105,10 +99,39 @@
 } 
 
 + (UIColor *) colorFromHexWithAlpha:(NSString *)hex:(float)alphaValue{
-	UIColor *color = [ColorFactory colorFromHex:hex];
-	[color setAlpha:alphaValue];
-	
-	return color;
+    float theAlpha = alphaValue;
+	if(theAlpha > 1.0f)
+        theAlpha = 1.0f;
+    
+    NSString *colorString = [[hex uppercaseString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] ;  
+    if ([colorString length] < 6) 
+        return [UIColor grayColor];  
+    
+    if ([colorString hasPrefix:@"0X"]) 
+        colorString = [colorString substringFromIndex:2];  
+    else if ([colorString hasPrefix:@"#"]) 
+        colorString = [colorString substringFromIndex:1]; 
+    else if ([colorString length] != 6) 
+        return  [UIColor grayColor];  
+    
+    NSRange range;  
+    range.location = 0;  
+    range.length = 2;  
+    NSString *rString = [colorString substringWithRange:range];  
+    range.location += 2;  
+    NSString *gString = [colorString substringWithRange:range];  
+    range.location += 2;  
+    NSString *bString = [colorString substringWithRange:range];  
+    
+    unsigned int red, green, blue;  
+    [[NSScanner scannerWithString:rString] scanHexInt:&red];  
+    [[NSScanner scannerWithString:gString] scanHexInt:&green];  
+    [[NSScanner scannerWithString:bString] scanHexInt:&blue];  
+    
+    return [UIColor colorWithRed:((float) red / 255.0f)  
+                           green:((float) green / 255.0f)  
+                            blue:((float) blue / 255.0f)  
+                           alpha:theAlpha];  
 }
 
 + (UIColor *) colorFromRGB:(int)red:(int)green:(int)blue{
@@ -119,10 +142,14 @@
 }
 
 + (UIColor *) colorFromRGBA:(int)red:(int)green:(int)blue:(float)alpha{
+    float theAlpha = alpha;
+    if(theAlpha > 1.0f)
+        theAlpha = 1.0f;
+    
     return [UIColor colorWithRed:((float) red / 255.0f)  
                            green:((float) green / 255.0f)  
                             blue:((float) blue / 255.0f)  
-                           alpha:alpha];  
+                           alpha:theAlpha];  
 }
 
 @end
